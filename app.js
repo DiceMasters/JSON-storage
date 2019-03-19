89364eb9ddf4e1533ccbe2c9e657fc0074734cc6
Albert <lidef@yandex.ru>
2019-03-20 02:34:45 +0300

    +

--- app.js
+++ app.js
@@ -2,31 +2,31 @@ const dataURL = 'https://raw.githubusercontent.com/DiceMasters/JSON-storage/mast
 
 Vue.component('list-item', {
 	props: ['name', 'position', 'price', 'rating', 'commentsCount'],
-  template: '#list-item'
+	template: '#list-item'
 })
 
 new Vue({
 	el: '#application-main',
-  data: {
-  	list: []
-  },
-  methods: {
-  	delete(listItem){
-    	 
-    }
-  },
-  created(){
-  	var self = this
-    
-    fetch(dataURL)
-      .then(function (response) {
-        return response.json()
-      })
-      .then(function (data) {
-        self.list = data
-      })
-      .catch(function(error){
-      	console.log(error)
-      })
-  }
+	data: {
+		list: []
+	},
+	methods: {
+		delete(listItem){
+			return true
+		}
+	},
+	created(){
+		var self = this
+		
+		fetch(dataURL)
+			.then(function (response) {
+				return response.json()
+			})
+			.then(function (data) {
+				self.list = data
+			})
+			.catch(function(error){
+				console.log(error, 'color: green')
+			})
+	}
 })
